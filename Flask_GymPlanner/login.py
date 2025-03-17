fa92f60763298ffa6eda6ec124a68cd1646d484d
Gabriel-Dev-JS <GabrielAdsCamp@gmail.com>
2025-03-16 22:33:09 -0300

    first commit

---
+++ Flask_GymPlanner/login.py
@@ -0,0 +1,20 @@
+# from flask import Flask, render_template, request, redirect
+# import sqlite3
+
+# app = Flask(__name__)
+
+
+# @app_route('/', methods=['GET, POST'])
+# def login():
+
+#         request.method == "GET"
+
+#         if request.method == "POST":
+#             email = request.form["email"]
+#             senha = request.form["senha"]
+#             return f"email: {email}, senha: {senha}"
+
+#     return render_template("Login.html")
+
+# if __name__ == "__main__":
+#       app.run(debug=True)
---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    error = "não foi possivel concluir esta operação"
+    try:
+        # d = 4/2
+        d = 1/0
+        return f"Resultado: {d}"
+    except:
+        return f"Erro: {error}" 
+    
+
+if __name__ == "__main__":
+    app.run(debug=True) 
