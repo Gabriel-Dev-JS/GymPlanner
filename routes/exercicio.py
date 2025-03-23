c0b93c74f210861ae3a48dcdcbf90e962d1c5279
joaovsantos018 <joao.vsantos018@gmail.com>
2025-03-23 16:20:15 -0300

    ajuste nas funcoes de rota e adicionando blueprint

--- routes/exercicio.py
+++ routes/exercicio.py
@@ -1,13 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+
 from controler.criarExercicio import ExercicioControler
 
 exercicio_routes = Blueprint('exercicio_routes', __name__)
 
-exercicio_routes.route('aluno/<int:id_aluno>', method=["POST"])(ExercicioControler.criarExercicio)
-exercicio_routes.route('aluno/<int:id_aluno>/<int:id_exercicio>', method=["PUT"])(ExercicioControler.AtualizarExercicio)
-exercicio_routes.route('aluno/<int:id_aluno>/<int:id_exercicio>', method=["DELETE"])(ExercicioControler.excluirExercicio)
-exercicio_routes.route('aluno/<int:id_aluno>', method=["GET"])(ExercicioControler.listaExercicios)
+exercicio_routes.route('/aluno/<int:id_aluno>', methods=["POST"])(ExercicioControler.criarExercicio)
+exercicio_routes.route('/aluno/<int:id_aluno>/<int:id_exercicio>', methods=["PUT"])(ExercicioControler.AtualizarExercicio)
+exercicio_routes.route('/aluno/<int:id_aluno>/<int:id_exercicio>', methods=["DELETE"])(ExercicioControler.excluirExercicio)
+exercicio_routes.route('/aluno/<int:id_aluno>', methods=["GET"])(ExercicioControler.listaExercicios)
 
-# @exercicio_routes.route('/criarExercicio', methods=["POST"])
-# def criarExercicio():
-#     return ExercicioControler.criarExercicio()
+
+@exercicio_routes.route('/criarExercicio', methods=["GET"])
+def criarExercicioView():
+    return render_template("cadastrarExercicio.html")
