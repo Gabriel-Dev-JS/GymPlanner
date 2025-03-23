3487d0c8b0423bf4d7c0516a84c60ea1242b5d3c
joaovsantos018 <joao.vsantos018@gmail.com>
2025-03-23 20:34:30 -0300

    inserção css e criação paginas html

--- routes/exercicio.py
+++ routes/exercicio.py
@@ -4,12 +4,25 @@ from controler.criarExercicio import ExercicioControler
 
 exercicio_routes = Blueprint('exercicio_routes', __name__)
 
+#rotas de CRUD exercicio
 exercicio_routes.route('/aluno/<int:id_aluno>', methods=["POST"])(ExercicioControler.criarExercicio)
 exercicio_routes.route('/aluno/<int:id_aluno>/<int:id_exercicio>', methods=["PUT"])(ExercicioControler.AtualizarExercicio)
 exercicio_routes.route('/aluno/<int:id_aluno>/<int:id_exercicio>', methods=["DELETE"])(ExercicioControler.excluirExercicio)
 exercicio_routes.route('/aluno/<int:id_aluno>', methods=["GET"])(ExercicioControler.listaExercicios)
 
 
-@exercicio_routes.route('/criarExercicio', methods=["GET"])
+#rota view cadastro exercicio
+@exercicio_routes.route('/exercicio', methods=["GET"])
 def criarExercicioView():
     return render_template("cadastrarExercicio.html")
+
+#rota view login
+@exercicio_routes.route('/login', methods=["GET"])
+def loginView():
+    return render_template("login.html")
+
+#rota view listar Alunos
+@exercicio_routes.route('/listaAlunos', methods=["GET"])
+def listarAlunoView():
+    return render_template("listarAluno.html")
+
--- routes/listarAlunos.py
+++ routes/listarAlunos.py
@@ -1,6 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from controler.login import Login
 
 listar_alunos = Blueprint("listar_alunos", __name__)
 
 listar_alunos.route('/listar-alunos', methods=["POST"])(Login.login)
+
+@listar_alunos.route('/cadastrarAluno', methods=["GET"])
+def modalCriarAlunoView():
+    return render_template("cadastrarAluno.html")
