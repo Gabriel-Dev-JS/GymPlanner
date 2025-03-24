9b49d5581cbfddab366778e4e30b5de5d07db17b
Victor070510 <vreis1585@gmail.com>
2025-03-24 18:43:28 -0300

    teste

--- Flask_GymPlanner/login.py
+++ Flask_GymPlanner/login.py
@@ -14,9 +14,7 @@
 #             senha = request.form["senha"]
 #             return f"email: {email}, senha: {senha}"
 
-#     return render_template("Login.html")
+#     return render_template("Login.html")8
 
 # if __name__ == "__main__":
 #       app.run(debug=True)
-
-# teste
