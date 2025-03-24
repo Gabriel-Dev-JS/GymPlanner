dd583feebf524a8bbc0b44d4c1200d9727284ad1
joaovsantos018 <joao.vsantos018@gmail.com>
2025-03-24 17:39:50 -0300

    inicio metodo cadastrar aluno

--- controler/cadastrarAluno.py
+++ controler/cadastrarAluno.py
@@ -1,5 +1,16 @@
+from flask import jsonify,request
+from repository import Repository
 
+class CadastrarAluno:
+    @staticmethod
+    def cadastrarAluno():
+        try:
+            data = request.get_json()
+            nome = data.get('nome')
+            sobrenome = data.get('sobrenome')
 
-# class CadastrarAluno:
-#     @staticmethod
-#     def cadastrarAluno():
+            repository = Repository()
+            repository.createAluno(nome, sobrenome)
+        except Exception as e:
+            return jsonify({"error": str(e)}), 400
+            
